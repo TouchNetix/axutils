17c53d47bdce0551c81abafd2503d22e0eca912d
Mark Satterthwaite <mark.satterthwaite@gmail.com>
2024-02-14 00:03:16 +0000

    Reworked axcfg to only perform downloads if the config file is compatible with the target device. This required reordering how the config file was parsed to extract the u31 and u33 contents first.

--- axcfg.py
+++ axcfg.py
@@ -30,6 +30,7 @@ import struct
 import argparse
 from time import sleep
 from axiom_tc import axiom
+from axiom_tc import u31_DeviceInformation
 from axiom_tc import u33_CRCData
 
 def init_axiom_comms(interface):
@@ -63,6 +64,41 @@ def init_axiom_comms(interface):
     # provide access to the low level reads and write methods.
     return axiom(comms)
 
+def extract_usages_from_config_file(config_file):
+    cfg_file_size = 0
+    usages = {}
+
+    try:
+        with open(config_file, "rb") as file:
+            cfg_file_size = os.path.getsize(config_file)
+
+            # The first 4 bytes of the file contains a signature that we use to
+            # identify the file as an aXiom config file.
+            signature = struct.unpack(">I", file.read(4))[0]
+            if signature == 0x20071969:
+                # Skip to offset 13 (from start of file) to skip over the header and
+                # get straight to the usage contents.
+                file.seek(13)
+
+                # Keep decoding the file until the end of file position is reached.
+                while True:
+                    # Extract the usage contents from the config file
+                    usage, revision, _, length = struct.unpack("<BBBH", file.read(5))
+                    usage_content = list(struct.unpack("<"+str(length)+"B", file.read(length)))
+
+                    # Store the contents into the usages list
+                    usages[usage] = (usage, revision, usage_content)
+
+                    # Check if the end of file has been reached
+                    if file.tell() == cfg_file_size:
+                        break
+    except FileNotFoundError:
+        print("Config file not found: " + config_file)
+    except IOError:
+        print("An error occurred while accessing the config file")
+
+    return cfg_file_size, usages
+
 if __name__ == '__main__':
     exit_code = 0
 
@@ -80,97 +116,86 @@ if __name__ == '__main__':
     config_file = args.f
     overwrite_u04 = args.c
 
+    # Initialise comms with axiom 
     ax = init_axiom_comms(args.i)
 
+    # Before we start, show the connected device's info
     ax.u31.print_device_info()
-
-    print("u33 CRC Data Before Config Load:")
+    
+    # Read u33 from the device to validate the config file is compatible later
     u33 = u33_CRCData(ax)
-    u33.print()
 
-    # Just init a u33 object to represent the u33 that will be in the file
+    # Extract out of the config file all the usages into a dictionary.
+    cfg_file_size, usages = extract_usages_from_config_file(config_file)
+    
+    # Before the new config is written to the device, check that the config file
+    # is compatible with the device by comparing the runtime CRC in u33. Manually
+    # populate the u33 content with the content from the file so that a compatibilty
+    # check can be performed
     u33_from_file = u33_CRCData(ax, False)
+    u33_from_file._usage_revision = usages[0x33][1]
+    u33_from_file._usage_binary_data = usages[0x33][2]
+    u33_from_file.init_usage()
+    u33_from_file._unpack()
 
-    if len(config_file) != 0:
-        # Tell aXiom to stop making measurement so that the config file can be
-        # loaded.
-        ax.u02.send_command(ax.u02.CMD_STOP)
-    
-        # Parse the config file and write the contents to aXiom
-        with open(config_file, "rb") as file:
-            # The first 4 bytes of the file contains a signature that we use to
-            # identify the file as an aXiom config file.
-            signature = struct.unpack(">I", file.read(4))[0]
-    
-            if signature == 0x20071969:
-                version = struct.unpack(">H", file.read(2))[0]
-    
-                # Seek to the end of the file to determine how long the file is.
-                file.seek(0, os.SEEK_END)
-                eof_pos = file.tell()
-    
-                # Skip to offset 13 (from start of file) to skip over the header and
-                # get straight to the usage contents.
-                file.seek(13)
-    
-                # Keep decoding the file until the end of file position is reached.
-                while True:
-                    # Extract the usage contents from the config file
-                    usage, revision, profile = struct.unpack(">BBB", file.read(3))
-                    # Length field is MSB (why!?)
-                    length = struct.unpack("<H", file.read(2))[0]
-                    usage_content = list(struct.unpack("<"+str(length)+"B", file.read(length)))
+    # Similarly, if the config file that was specified is incompatible, the details
+    # would be in u31. Capture that here, but it will only be used if the config file
+    # is not compatible. This information will tell us what firmware the config came from
+    u31_from_file = u31_DeviceInformation(ax)
+    u31_from_file._usage_revision = usages[0x31][1]
+    u31_from_file._usage_binary_data = usages[0x31][2]
+    u31_from_file._unpack()
 
-                    # Write the data to the device
-                    if overwrite_u04:
-                        ax.config_write_usage_to_device(usage, usage_content)
-                    else:
-                        if usage != 0x04:   # u04 contains customer populated data, such as serial numbers, part numbers
-                                            # serial numbers, etc. Therefore it is assumed that the customer would want 
-                                            # to keep the data they have input unless otherwise specified.
-                            ax.config_write_usage_to_device(usage, usage_content)
-    
-                    # Capture u33 from the config file which contains the CRCs
-                    if usage == 0x33:
-                        u33_from_file._usage_revision = revision
-                        u33_from_file._usage_binary_data = usage_content
-                        u33_from_file._unpack()
-    
-                    progress = (float(file.tell()) / float(eof_pos)) * 100
-                    sys.stdout.write('\033[?25l') # Hide cursor
-                    sys.stdout.write("Progress: %3.0f%%\r" % (progress))
-                    sys.stdout.flush()
-                    sys.stdout.write('\033[?25h') # Show cursor
-    
-                    # Check if the end of file has been reached
-                    if file.tell() == eof_pos:
-                        print("")
-                        print("")
-                        break
-            else:
-                print("ERROR: The specified config file is not recognised")
-                sys.exit(-1)
-    
-    
-        # The CDUs will already have had their contents stored to NVM, but the main
-        # usages are still only in RAM. Issusing this command to commit them to
-        # NVM.
-        ax.u02.send_command(ax.u02.CMD_SAVE_CONFIG)
-    
-        # Give aXiom some time to write the contents to NVM. There might be a more
-        # sophisticated way to do this than just a sleep.
-        sleep(2)
-    
-        # Restart aXiom
-        ax.u02.send_command(ax.u02.CMD_SOFT_RESET)
-        sleep(1)
-    
-        # Verify if the contents have been loaded onto the device correctly
-        u33 = u33_CRCData(ax)
-        config_loaded_successfully = u33.compare_u33(u33_from_file, True)
-        if config_loaded_successfully == False:
-            print("FAILED to load config file correctly.")
-            exit_code = -1
+    # Compare the firmware runtime CRC from the file with the CRC from the device.
+    # Only proceed if the CRCs match.
+    if u33_from_file.reg_runtime_crc != u33.reg_runtime_crc:
+        print("")
+        print("ERROR: Cannot load config file as it was saved from a different revision of firmware.")
+        print("       Firmware info from device      : 0x{0:08X}, {1}".format(u33.reg_runtime_crc, ax.u31.get_device_info_short()))
+        print("       Firmware info from config file : 0x{0:08X}, {1}".format(u33_from_file.reg_runtime_crc, u31_from_file.get_device_info_short()))
+        sys.exit(-1)
+
+    # Stop axiom from performing measurements whilst loading the config file
+    ax.u02.send_command(ax.u02.CMD_STOP)
+
+    # Write the config to the device by iterating over all the usages from the config file
+    bytes_written = 0
+    for usage in usages:
+        usage_rev     = usages[usage][1]
+        usage_content = usages[usage][2]
+
+        # u04 contains customer populated data, such as serial numbers, part numbers
+        # serial numbers, etc. Therefore it is assumed that the customer would want 
+        # to keep the data they have input unless otherwise specified (-c option).
+        if usage != 0x04 or (usage == 0x04 and overwrite_u04):
+            ax.config_write_usage_to_device(usage, usage_content)
+
+        bytes_written += len(usage_content)
+        progress = (float(bytes_written) / float(cfg_file_size)) * 100
+        sys.stdout.write('\033[?25l') # Hide cursor
+        sys.stdout.write("Progress: %3.0f%%\r" % (progress))
+        sys.stdout.flush()
+        sys.stdout.write('\033[?25h') # Show cursor
+
+    # The CDUs will already have had their contents stored to NVM, but the main
+    # usages are still only in RAM. Issusing this command to commit them to
+    # NVM.
+    ax.u02.send_command(ax.u02.CMD_SAVE_CONFIG)
+
+    # Give aXiom some time to write the contents to NVM.
+    sleep(2)
+
+    # Restart aXiom
+    ax.u02.send_command(ax.u02.CMD_SOFT_RESET)
+    sleep(1)
+
+    # Verify if the contents have been loaded onto the device correctly by re-reading
+    # u33 from the device and comparing it with the file
+    u33 = u33_CRCData(ax)
+    config_loaded_successfully = u33.compare_u33(u33_from_file, True)
+    if config_loaded_successfully == False:
+        print("ERROR: Failed to load config file")
+        exit_code = -1
 
     # Safely close the connection to aXiom
     ax.close()
